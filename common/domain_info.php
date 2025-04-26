7beb63751e8e264db478cbf64573b96acae84a12
rikitaguchi <riki_iphone2@icloud.com>
2025-04-26 13:19:05 +0900

    app set up

--- common/domain_info.php
+++ common/domain_info.php
@@ -1,8 +1,8 @@
 <?php
 if ($_SERVER['SERVER_NAME'] === 'localhost') {
-    // 本番環境
-    $domain = 'https://wordsystemforstudents.com';
-} else {
     // ローカル環境
     $domain = 'http://localhost/learning-support-app';
+} else {
+    // 本番環境
+    $domain = 'https://wordsystemforstudents.com';
 }
