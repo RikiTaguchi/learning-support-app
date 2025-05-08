3e8628f7a3db10d1c7b3c309b21b7afafc274810
rikitaguchi <riki_iphone2@icloud.com>
2025-05-08 14:13:20 +0900

    set login streak

--- h/header.php
+++ h/header.php
@@ -10,9 +10,14 @@
             <p>高校生版</p>
         </button>
     </form>
-    <button class = "header-menu-button">
-        <img src = "../common/images/menu.svg" alt = "メニューボタン">
-    </button>
+    <div class="header-right-area">
+        <div>
+            <?php echo $login_streak; ?> 📚
+        </div>
+        <button class = "header-menu-button">
+            <img src = "../common/images/menu.svg" alt = "メニューボタン">
+        </button>
+    </div>
     <p class = "header-menu-info" style = "display: none;">closed</p>
     <div class = "header-site-menu">
         <ul>
